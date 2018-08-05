dcbc122a914deb509710a6dde712de536f01a574
akshay-98 <akshay.prasanakumar98@gmail.com>
2018-08-05 20:19:32 +0530

    app1

--- app/src/main/java/com/example/evillageapp/onlineBidActivity.java
+++ app/src/main/java/com/example/evillageapp/onlineBidActivity.java
@@ -1,13 +1,45 @@
 package com.example.evillageapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class onlineBidActivity extends AppCompatActivity {
+
+    Spinner durationSpinner;
+
+    public void submit(View view)
+    {
+        Log.i("info","submit button pressed");
+        Intent placeBid=new Intent(onlineBidActivity.this,bidPlacedActivity.class);
+        this.startActivity(placeBid);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_bid);
+
+
+
+        ArrayList<Integer> durationList=new ArrayList<Integer>();
+        durationList.add(12);
+        durationList.add(24);
+        durationList.add(48);
+
+        durationSpinner=(Spinner)findViewById(R.id.durationSpinner);
+
+        ArrayAdapter<Integer> durationAdapter=new ArrayAdapter<Integer>(this,android.R.layout.simple_spinner_item,durationList);
+        durationSpinner.setAdapter(durationAdapter);
+        durationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
     }
 }
